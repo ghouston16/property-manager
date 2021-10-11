5006f950b8fba8458c6660ac5282aa99f3af8cd3
ghouston16 <ghouston16@hotmail.com>
2021-10-11 22:05:01 +0100

    main app added

--- app/src/main/java/org/wit/property_manager/activities/PropertyActivity.kt
+++ app/src/main/java/org/wit/property_manager/activities/PropertyActivity.kt
@@ -5,31 +5,34 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import org.wit.property_manager.R
 import org.wit.property_manager.databinding.ActivityPropertyBinding
+import org.wit.property_manager.main.MainApp
 import org.wit.property_manager.models.PropertyModel
-
 import timber.log.Timber
 import timber.log.Timber.i
+
 class PropertyActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPropertyBinding
     var property = PropertyModel()
-    val properties = ArrayList<PropertyModel>()
+ //   val properties = ArrayList<PropertyModel>()
+    lateinit var app : MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPropertyBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Timber.plant(Timber.DebugTree())
+        app = application as MainApp
+     //   Timber.plant(Timber.DebugTree())
         i("Property Activity started...")
 
         binding.btnAdd.setOnClickListener() {
             property.title = binding.propertyTitle.text.toString()
             property.description =binding.propertyDescription.text.toString()
             if (property.title.isNotEmpty()) {
-                properties.add(property.copy())
+                app.properties.add(property.copy())
                 i("add Button Pressed: ${property}")
-                for (i in properties.indices)
-                { i("Property[$i]:${this.properties[i]}") }
+                for (i in app.properties.indices)
+                { i("Property[$i]:${this.app.properties[i]}") }
             }
             else {
                 Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
---
+++ app/src/main/java/org/wit/property_manager/main/MainApp.kt
@@ -0,0 +1,16 @@
+package org.wit.property_manager.main
+
+import android.app.Application
+import org.wit.property_manager.models.PropertyModel
+import timber.log.Timber
+import timber.log.Timber.i
+
+class MainApp : Application() {
+    val properties = ArrayList<PropertyModel>()
+    override fun onCreate(){
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        i("Property App started")
+    }
+
+}
